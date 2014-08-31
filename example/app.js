773bc72951a01a49032b1e68d124788f96796878
wanglihui <lihui.wang@tulingdao.com>
2014-08-31 22:31:27 +0800

    初始化项目

---
+++ example/app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var redis = require('redis');
+var RedisStore = require('connect-redis')(expressSession);
+var rClient = redis.createClient();
+var sessionStore = new RedisStore({client:rClient});
+
+var routes = require('./routes');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(expressSession({secret:"example.com", store: sessionStore}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+var User = require('./model/user');
+config = {
+    userModel:User
+}
+var AuthManager = require('../lib').AuthManager;
+var authManager = new AuthManager(config);
+
+authManager.registerRouter(app);
+app.get('/', routes.index);
+app.get('/list',authManager.requireAuthenticate(), function(req, res, next){
+    res.send("list");
+});
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ example/model/user.js
@@ -0,0 +1,37 @@
+/**
+ * Created by think on 2014/8/31.
+ */
+var UserMin = require('../../lib').UserMin;
+
+function User(){
+    UserMin.call(this);
+}
+
+User.prototype = new UserMin();
+
+User.prototype.loadUser = function(id, callback) {
+    if (id) {
+        var user = new User();
+        user.id = id;
+        user.username = 'test';
+        user.pwd = 'test';
+        callback(null, user);
+    } else {
+        var err = new Error("id is empty!");
+        callback(err);
+    }
+}
+
+User.prototype.authentication = function(username, pwd, callback) {
+    if (username == 'test' && pwd == 'test') {
+        var user = new User();
+        user.username = 'test';
+        user.pwd = 'test';
+        user.id = 1;
+        callback(null, user);
+    } else {
+        callback(null, null);
+    }
+}
+
+module.exports = User;
